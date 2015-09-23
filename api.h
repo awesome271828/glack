b6e3a3a5cf72bf37b7df995378d67742f68b58f2
Devin Akman <akman2@illinois.edu>
2015-09-23 02:01:13 +0000

    included usage for glack_API_call()

--- api.h
+++ api.h
@@ -9,6 +9,7 @@ glackAPIHandle glack_API_init(void);
 
 void glack_API_cleanup(glackAPIHandle handle);
 
+// glack_API_call(handle, method, key1, value1, key2, value2, ..., NULL);
 char *glack_API_call(glackAPIHandle handle, const char *method, ...);
 
 #endif
